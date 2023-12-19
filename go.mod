f2a22c950474d915a00d4233a367b76d48aa0483
Kieran <kieran.webdevelopment@gmail.com>
2023-12-19 14:25:29 +0000

    added helper functions

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/KieranWebDev/Go-BlogAggregator
 go 1.21.4
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
 	github.com/joho/godotenv v1.5.1
+    github.com/go-chi/chi/v5 v5.0.10
 )
+
--- main.go
+++ main.go
@@ -1,36 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-
 	godotenv.Load(".env")
 
-	portString := os.Getenv("PORT")
-	if portString == "" {
-		log.Fatal("PORT is not found in the environment variables")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
 	}
-	fmt.Println("PORT", portString)
 
 	router := chi.NewRouter()
 
-	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
-	}
-	fmt.Printf("Starting server at port %s\n", portString)
-	srv.ListenAndServe()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
 	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
